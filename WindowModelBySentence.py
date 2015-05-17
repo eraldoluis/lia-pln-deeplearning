- Removendo código utilizado para debug - Arrumando questão da estabilidade numérica no softmax regular. - Arrumando bug que permitia que steps do scan, que calcula o logadd de   todos os caminhos, fosse igual a zero. - Arrumando bug que não lia todo o conjunto de treinamento

WindowModelBySentence.py
@@ -7,7 +7,6 @@ from WindowModelBasic import WindowModelBasic
 import numpy as np
 from itertools import chain
 
-
 class WindowModelBySentence(WindowModelBasic):
 
     def __init__(self, lexicon, wordVectors , windowSize, hiddenSize, _lr,numClasses,numEpochs, batchSize=1, c=0.0,):
@@ -19,10 +18,7 @@ class WindowModelBySentence(WindowModelBasic):
         # Custo
         parameters = self.sentenceSoftmax.getParameters() + self.hiddenLayer.getParameters()
         
-        numWords = self.windowIdxs.shape[0]
-        
         logOfSumAllPath = self.sentenceSoftmax.getLogOfSumAllPathY()
-        
         negativeLogLikehood = -(self.sentenceSoftmax.getSumPathY(self.y) - logOfSumAllPath)
         cost =   negativeLogLikehood + regularizationSquareSumParamaters(parameters, self.regularizationFactor, self.y.shape[0]);
                     
@@ -50,7 +46,7 @@ class WindowModelBySentence(WindowModelBasic):
         
         return np.array(allWindowIndexes);
     
-    def confBatchSize(self,numWordsInTrain):
+    def confBatchSize(self,inputData):
         # Configura o batch size
         return np.asarray(self.setencesSize)
     

WindowModelByWord.py
@@ -3,10 +3,6 @@
 
 import numpy as np
 import theano
-import theano.tensor as T
-from NNet.HiddenLayer import HiddenLayer
-from NNet.WortToVectorLayer import WordToVectorLayer
-from theano.tensor.nnet.nnet import softmax
 from NNet.SoftmaxLayer import SoftmaxLayer
 from NNet.Util import negative_log_likelihood, regularizationSquareSumParamaters
 from WindowModelBasic import WindowModelBasic
@@ -45,14 +41,14 @@ class WindowModelByWord(WindowModelBasic):
             
         return np.array(allWindowIndexes);
     
-    def confBatchSize(self,numWordsInTrain):
+    def confBatchSize(self,inputData):
+        numWords = len(inputData)
+        
         # Configura o batch size
         if isinstance(self.batchSize, list):
             return np.asarray(self.batchSize);
         
-        
-        
-        return np.full(numWordsInTrain/self.batchSize + 1,self.batchSize)
+        return np.full(numWords/self.batchSize + 1,self.batchSize)
         
     def predict(self, inputData):    
         self.reloadWindowIds = True
b1cd6c95f41a4589af014f887d341e14abbaaf56
Eraldo R. Fernandes <eraldo@facom.ufms.br>
2016-07-27 09:28:42 -0400

    Criei MaxPoolingLayer e ReshapeLayer.

---
+++ nnet/MaxPoolingLayer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import theano.tensor as T
+
+from nnet.Layer import Layer
+
+
+class MaxPoolingLayer(Layer):
+
+    def __init__(self,_input):
+        super(MaxPoolingLayer, self).__init__(_input)
+
+        # We apply a max operator along the first dimension.
+        self.__output = T.max(self.getInput(), axis=0)
+
+    def getOutput(self):
+        return self.__output
+
+    def getParameters(self):
+        return []
+
+    def getStructuredParameters(self):
+        return []
+
+    def getDefaultGradParameters(self):
+        return []
+
+    def getUpdates(self, cost, learningRate, sumSqGrads=None):
+        return []
